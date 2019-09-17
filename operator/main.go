e74bc81f9463cce191827ad199af072c5995c970
dhanan <dhanan_richy@yahoo.com>
2019-09-17 15:18:01 +0700

    update operator aritmatic tutorial

--- operator/main.go
+++ operator/main.go
@@ -27,4 +27,22 @@ func main()  {
   //pembagian / divides
   zDiv := x / y
   fmt.Println("x / y =",zDiv)
+
+  //modulus
+  zMod := x % y
+  fmt.Println("x % y =",zMod)
+  fmt.Println("x / y =",zDiv)
+
+  //relational operator
+  i := 4
+  j := 2
+  fmt.Println("i =",i)
+  fmt.Println("j =",j)
+
+  fmt.Println("i == j =",i == j) //equals
+  fmt.Println("i != j =",i != j) //not equalse
+  fmt.Println("i > j =",i > j) //grether than
+  fmt.Println("i < j =",i < j) //less then
+  fmt.Println("i >= j =",i >= j) //grether than
+  fmt.Println("i <= j =",i <= j) //less then
 }
